312d01fb2d2b3ebe43fbd2be4cde0cc00f1503d7
tempiltin <temurbekshukurov0707@gmail.com>
2021-01-19 00:00:00 +0500

    Commit on 2021-01-19T00:00:00

--- C++/my_project_36.cpp
+++ C++/my_project_36.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 using namespace std;
-void selectionSort(int arr[], int n) {
-    for(int i = 0; i < n - 1; i++) {
-        int min_idx = i;
-        for(int j = i + 1; j < n; j++)
-            if(arr[j] < arr[min_idx])
-                min_idx = j;
-        swap(arr[min_idx], arr[i]);
-    }
+void bubbleSort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++)
+        for(int j = 0; j < n - i - 1; j++)
+            if(arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
 }
 int main() {
-    int arr[] = {10, 20, 5, 7, 15};
+    int arr[] = {5, 3, 8, 6, 2, 7};
     int n = sizeof(arr)/sizeof(arr[0]);
-    selectionSort(arr, n);
+    bubbleSort(arr, n);
     for(int i = 0; i < n; i++)
         cout << arr[i] << " ";
     return 0;
