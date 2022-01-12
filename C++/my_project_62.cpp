49b238c8548819361aedcf06a5e8fea78d5f094e
tempiltin <temurbekshukurov0707@gmail.com>
2022-01-13 00:00:00 +0500

    Commit on 2022-01-13T00:00:00

--- C++/my_project_62.cpp
+++ C++/my_project_62.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 int main() {
-    time_t now = time(0);
-    tm *ltm = localtime(&now);
-    cout << "Time: " << 1 + ltm->tm_hour << ":" 
-         << 1 + ltm->tm_min << ":" 
-         << 1 + ltm->tm_sec << endl;
+    double a, b;
+    char op;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> b;
+    switch(op) {
+        case '+': cout << "Result: " << a + b; break;
+        case '-': cout << "Result: " << a - b; break;
+        case '*': cout << "Result: " << a * b; break;
+        case '/': cout << (b != 0 ? a / b : 0); break;
+        default: cout << "Invalid operator!";
+    }
     return 0;
 }
