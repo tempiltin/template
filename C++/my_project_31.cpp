8578f72807cd459a7c7f0de85944d94627ad7b35
tempiltin <temurbekshukurov0707@gmail.com>
2022-01-14 00:00:00 +0500

    Commit on 2022-01-14T00:00:00

--- C++/my_project_31.cpp
+++ C++/my_project_31.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
-void selectionSort(int arr[], int n) {
-    for(int i = 0; i < n - 1; i++) {
-        int min_idx = i;
-        for(int j = i + 1; j < n; j++)
-            if(arr[j] < arr[min_idx])
-                min_idx = j;
-        swap(arr[min_idx], arr[i]);
+void insertionSort(int arr[], int n) {
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
     }
 }
 int main() {
-    int arr[] = {10, 20, 5, 7, 15};
+    int arr[] = {9, 7, 5, 3, 1};
     int n = sizeof(arr)/sizeof(arr[0]);
-    selectionSort(arr, n);
+    insertionSort(arr, n);
     for(int i = 0; i < n; i++)
         cout << arr[i] << " ";
     return 0;
