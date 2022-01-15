b04016aefc65ae3ac72f3dac8c6fca393187fba2
tempiltin <temurbekshukurov0707@gmail.com>
2022-01-16 00:00:00 +0500

    Commit on 2022-01-16T00:00:00

--- C++/my_project_45.cpp
+++ C++/my_project_45.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 using namespace std;
-void insertionSort(int arr[], int n) {
-    for(int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-        while(j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = key;
-    }
+void bubbleSort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++)
+        for(int j = 0; j < n - i - 1; j++)
+            if(arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
 }
 int main() {
-    int arr[] = {9, 7, 5, 3, 1};
+    int arr[] = {5, 3, 8, 6, 2, 7};
     int n = sizeof(arr)/sizeof(arr[0]);
-    insertionSort(arr, n);
+    bubbleSort(arr, n);
     for(int i = 0; i < n; i++)
         cout << arr[i] << " ";
     return 0;
