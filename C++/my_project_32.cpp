79c221c99d6730cc11583947138878d8b8f6b7ff
tempiltin <temurbekshukurov0707@gmail.com>
2022-01-24 00:00:00 +0500

    Commit on 2022-01-24T00:00:00

--- C++/my_project_32.cpp
+++ C++/my_project_32.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
+void selectionSort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        for(int j = i + 1; j < n; j++)
+            if(arr[j] < arr[min_idx])
+                min_idx = j;
+        swap(arr[min_idx], arr[i]);
+    }
+}
 int main() {
-    time_t now = time(0);
-    tm *ltm = localtime(&now);
-    cout << "Time: " << 1 + ltm->tm_hour << ":" 
-         << 1 + ltm->tm_min << ":" 
-         << 1 + ltm->tm_sec << endl;
+    int arr[] = {10, 20, 5, 7, 15};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    selectionSort(arr, n);
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     return 0;
 }
